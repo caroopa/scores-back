9a33e55e1894a74f9bc20d6c35d878a9025a3d61
Carolina Denisse Paleari <palearicaro@gmail.com>
2024-06-29 01:00:27 -0300

    Postgre connection.

--- database.py
+++ database.py
@@ -1,17 +1,22 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "sqlite:///./test.db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 metadata = MetaData()
 
+
 def get_db():
-  db = SessionLocal()
-  try:
-      yield db
-  finally:
-      db.close()
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
